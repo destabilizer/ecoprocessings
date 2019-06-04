39819ada2afba4fb7f52fb9aa2cc1e7870543415
Balaram Usov <dest@disroot.org>
2019-06-04 15:57:19 +0300

    Added sesnors.py with experimental correlation between amperka sensor and other

---
+++ sensors.py
@@ -0,0 +1,36 @@
+AMPERKA_GROVE_CORRELATION = [-2.62990714,  1.68021727]
+
+import numpy
+
+def amperka_light(dataval):
+    if not (0 < dataval < 1): return
+    old_light_value = dataval * 102.4
+    sensorADC = int(round(old_light_value * 32))
+    sensorADC = sensorADC >> 5
+    sensorRatio = 1024 / sensorADC - 1
+    sensorResistanse = 10000 / sensorRatio
+    light_value = 32017200 / sensorResistanse ** 1.5832
+    light_value = round(light_value, 2)
+    return light_value
+
+def grove_light(dataval):
+    t = numpy.polyval(AMPERKA_GROVE_CORRELATION, dataval)
+    #print(dataval, '->', t)
+    return amperka_light(t)
+
+def process_datatype(name, suffix, func, datadict):
+    processed = func(datadict[name])
+    return (name+'_'+suffix, processed)
+
+def process_data(dbname):
+    from pymongo import MongoClient
+    mc = MongoClient()
+    db = mc[dbname]
+    for cn in db.list_collection_names():
+        col =  db[cn]
+        for du in col.find():
+            new_values = list()
+            new_values.append(process_datatype('light_at_top', 'lux', amperka_light, du))
+            new_values.append(process_datatype('light', 'lux', grove_light, du))
+            col.update_one({'_id': du['_id']}, {'$set': dict(new_values)})
+            
