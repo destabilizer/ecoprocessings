eb02228707515ef3b5c994d1564585c27b38b828
Balaram Usov <dest@disroot.org>
2019-06-05 03:27:39 +0300

    added model.py with tools for building models based on experimental data

---
+++ model.py
@@ -0,0 +1,57 @@
+from pymongo import MongoClient
+import numpy as np
+from scipy import stats
+import matplotlib.pyplot as plt
+
+graph_name = str()
+graph_axis_names = list()
+X, Y = list(), list()
+model_func = None
+model_name = str()
+axis = [0, 1, 0, 1]
+r2 = None
+
+def add_data(field_x, field_y, dbname, filter_func=lambda x, y: True):
+    "Add data from database"
+    global graph_name, graph_params
+    graph_name = dbname
+    graph_axis_names.extend((field_x, field_y))
+    global X, Y
+    mc = MongoClient()
+    db = mc[dbname]
+    col = db[db.list_collection_names()[0]]
+    X, Y = list(), list()
+    for d in col.find():
+        x, y = d[field_x], d[field_y]
+        if filter_func(x, y):
+            X.append(x)
+            Y.append(y)
+
+def build_linear():    
+    global model_func, model_name, r2
+    model_name = 'linear'
+    slope, intercept, r_value, p_value, std_err = stats.linregress(X,Y)
+    coeff = (slope, intercept)
+    model_func = lambda x: np.polyval(coeff, x)
+    r2 = r_value**2
+    return coeff
+
+def build_quad():
+    global model_func, model_name, r2
+    model_name = 'quad'
+    coeff = np.polyfit(X, Y, 2)
+    model_func = lambda x: np.polyval(coeff, x)
+    return coeff
+
+def draw_plot():
+    xtest = np.arange(axis[0], axis[1], (axis[1]-axis[0])/100)
+    ytest = model_func(xtest)
+    plt.plot(X, Y, 'bo')
+    plt.plot(xtest, ytest, 'r')
+    plt.axis(axis)
+    plt.xlabel(graph_axis_names[0])
+    plt.ylabel(graph_axis_names[1])
+    plt.text(0, 0, 'R^2='+str(r2))
+    plt.savefig(graph_name+'_'+model_name+'.png')
+    plt.clf()
+
